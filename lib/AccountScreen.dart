fe42b4125281949912131479d0b697bcd945b538
AoraveeD <54354162+AoraveeD@users.noreply.github.com>
2021-03-07 19:03:23 +0700

    Add files via upload

---
+++ lib/AccountScreen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/Login.dart';
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+        decoration: BoxDecoration(
+        gradient: LinearGradient(
+        colors: [Colors.blue, Colors.blueGrey],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter)
+    ),
+    child: MyStatelessWidget(),)
+    );
+  }
+  }
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/iceberg.png',
+              height: 300,
+              width: 300,
+            ),
+            RaisedButton(
+              //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+              onPressed: () {},
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30))),
+
+              // padding: const EdgeInsets.all(20),
+              child: const Text('Sign Up', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginDemo()),
+                );
+              },
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30))),
+
+              // padding: const EdgeInsets.all(20),
+              child: const Text('Login', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+              onPressed: () {},
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30))),
+
+              // padding: const EdgeInsets.all(20),
+              child: const Text(
+                'Login as Guest',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+}
